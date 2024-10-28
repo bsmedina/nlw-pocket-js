8b453f64907cf071fc3b7fe69b80678935d9b432
Beatriz Sampaio Medina <bee.a2@hotmail.com>
2024-10-28 19:18:23 -0300

    Configuracoes backend
    Add configuracoes referente ao db, logicas e validacoes para os endpoints

---
+++ src/http/server.ts
@@ -0,0 +1,33 @@
+import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { createGoalRoute } from './create-goal'
+import { getPendingGoalsRoute } from './get-pending-goals'
+import { createCompletionsRoute } from './create-completion'
+import { getWeekSummaryRoute } from './get-week-summary'
+import fastifyCors from '@fastify/cors'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.register(fastifyCors, {
+  origin: '*'
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createGoalRoute)
+app.register(createCompletionsRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running')
+  })
